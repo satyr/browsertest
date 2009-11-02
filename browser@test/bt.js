6d5923bb6e87f0999d0afc9ae2a7859d42ea9cf8
satyr <murky.satyr@gmail.com>
2009-11-02 10:59:32 +0900

    button/optionsURL

--- browser@test/bt.js
+++ browser@test/bt.js
@@ -1,22 +1,36 @@
-const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+var {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 var IOS = Cc['@mozilla.org/network/io-service;1'].getService(Ci.nsIIOService);
-var RPH = (IOS.getProtocolHandler('resource')
-           .QueryInterface(Ci.nsIResProtocolHandler));
-var B = document.getElementById('test-browser');
-var rurl = 'resource://bt/bt.htm';
-var urls = [
-  rurl,
-  RPH.resolveURI(IOS.newURI(rurl, null, null)),
-  'data:text/html,bt',
-  'chrome://bt/content/bt.htm',
-  ];
-urls.sort(function() Math.random() - .5);
-B.addEventListener('load', function(){
-  var {location} = B.contentWindow;
-  location.href =
-    'javascript:dump("'+ location.protocol +' "+window.Application+"\\n")';
-  var u = urls.shift();
-  if(u) setTimeout(function(){ B.setAttribute('src', u) });
-}, false);
-B.setAttribute('src', urls.shift());
-dump('\n');
+if(location.search === '?options'){
+  let uri = IOS.newURI(/^[^?]+/(location)[0], null, null);
+  Application.activeWindow.open(uri).focus();
+  close();
+}
+function test(ev){
+  var button = this;
+  button.disabled = true;
+  var RPH = (IOS.getProtocolHandler('resource')
+             .QueryInterface(Ci.nsIResProtocolHandler));
+  var B = document.getElementById('test-browser');
+  var rurl = 'resource://bt/bt.htm';
+  var urls = [
+    rurl,
+    RPH.resolveURI(IOS.newURI(rurl, null, null)),
+    ('data:text/html,'+
+     '<body onload="with(document)write(typeof Application);close()">'),
+    'chrome://bt/content/bt.htm',
+    ];
+  urls.sort(function() Math.random() - .5);
+  B.addEventListener('load', function(){
+    var {location} = B.contentWindow;
+    location.href =
+      'javascript:dump("'+ location.protocol +' "+window.Application+"\\n")';
+    var u = urls.shift();
+    if(u) setTimeout(function(){ B.setAttribute('src', u) }, 99);
+    else {
+      B.removeEventListener('load', arguments.callee, false);
+      button.disabled = false;
+    }
+  }, false);
+  dump('\n'+ [/^\w+/(u) for each(u in urls)].join(' -> ') +'\n');
+  B.setAttribute('src', urls.shift());
+}
